bf72b5c76eeb8cee2dc7344ffe99756d092dcde1
Michael Wagner <mitch.wagna@gmail.com>
2019-01-02 12:39:08 +0100

    tests(messages): add midi message tests

--- app/src/test/java/org/mitchwork/midi/messages/ControlChangeMessageTest.kt
+++ app/src/test/java/org/mitchwork/midi/messages/ControlChangeMessageTest.kt
@@ -1,12 +1,17 @@
 package org.mitchwork.midi.messages
 
+import org.junit.Assert
 import org.junit.Test
-
-import org.junit.Assert.*
 
 class ControlChangeMessageTest {
 
     @Test
     fun toByteArray() {
+        val cc = ControlChangeMessage(12, 22, 125)
+
+        val bytes = cc.toByteArray()
+        val expected = byteArrayOf(0xBC.toByte(), 0x16.toByte(), 0x7D.toByte())
+
+        Assert.assertTrue("ControlChange message to bytes", expected.contentEquals(bytes))
     }
 }
--- app/src/test/java/org/mitchwork/midi/messages/ProgramChangeMessageTest.kt
+++ app/src/test/java/org/mitchwork/midi/messages/ProgramChangeMessageTest.kt
@@ -1,12 +1,17 @@
 package org.mitchwork.midi.messages
 
+import org.junit.Assert.assertTrue
 import org.junit.Test
-
-import org.junit.Assert.*
 
 class ProgramChangeMessageTest {
 
     @Test
     fun toByteArray() {
+        val pc = ProgramChangeMessage(12, 22)
+
+        val bytes = pc.toByteArray()
+        val expected = byteArrayOf(0xCC.toByte(), 0x16.toByte())
+
+        assertTrue("ProgramChange message to bytes", expected.contentEquals(bytes))
     }
 }
