58b504f0c0af0c1850a338fcaa69f5bd55e63bf0
Michael Wagner <mitch.wagna@gmail.com>
2019-01-04 15:22:56 +0100

    chore(midi): persist input port within viewmodel

--- app/src/main/java/org/mitchwork/midi/MidiDeviceDetailControlChangesFragment.kt
+++ app/src/main/java/org/mitchwork/midi/MidiDeviceDetailControlChangesFragment.kt
@@ -1,8 +1,10 @@
 package org.mitchwork.midi
 
 import android.content.Context
+import android.media.midi.MidiInputPort
 import android.media.midi.MidiManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,6 +18,9 @@ import org.mitchwork.midi.data.AppDatabase
 import org.mitchwork.midi.data.ControlChangeWithMidiChannel
 import org.mitchwork.midi.data.MidiDeviceRepository
 import org.mitchwork.midi.databinding.FragmentMidiDeviceDetailControlChangesBinding
+import org.mitchwork.midi.messages.ControlChangeMessage
+import org.mitchwork.midi.viewmodels.AvailableMidiDevicesViewModel
+import org.mitchwork.midi.viewmodels.AvailableMidiDevicesViewModelFactory
 import org.mitchwork.midi.viewmodels.MidiDeviceDetailViewModel
 import org.mitchwork.midi.viewmodels.MidiDeviceDetailViewModelFactory
 
@@ -24,6 +29,7 @@ class MidiDeviceDetailControlChangesFragment : Fragment() {
     private lateinit var viewModel: MidiDeviceDetailViewModel
     private lateinit var viewBinding: FragmentMidiDeviceDetailControlChangesBinding
     private lateinit var listAdapter: ControlChangeAdapter
+    private lateinit var activityViewModel: AvailableMidiDevicesViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,6 +48,10 @@ class MidiDeviceDetailControlChangesFragment : Fragment() {
         val repository = MidiDeviceRepository.getInstance(db.midiDeviceDao(), db.controlChangeDao(), midiManager)
         val factory = MidiDeviceDetailViewModelFactory(repository, deviceID)
 
+        activityViewModel = ViewModelProviders.of(
+            requireActivity(), AvailableMidiDevicesViewModelFactory(midiManager)
+        ).get(AvailableMidiDevicesViewModel::class.java)
+
         viewModel = ViewModelProviders.of(this, factory).get(MidiDeviceDetailViewModel::class.java)
 
         listAdapter = ControlChangeAdapter(object : ControlChangeAdapter.OnControlChangeValueChangedListener {
@@ -59,12 +69,54 @@ class MidiDeviceDetailControlChangesFragment : Fragment() {
             it.findNavController().navigate(direction)
         }
 
+        activityViewModel.midiDevice?.observe(viewLifecycleOwner, Observer { midiDevice ->
+            Log.i("MIDI", "Midi device open: $midiDevice")
+            if (midiDevice.info.inputPortCount != 0) {
+                activityViewModel.openFirstInputPort()
+            } else {
+                Log.w("MIDI", "device has no input port: ${midiDevice.info.ports}")
+            }
+        })
+
+        activityViewModel.midiInputPort?.observe(viewLifecycleOwner, MidiInputPortObserver(listAdapter))
+
         viewModel.controlChanges.observe(viewLifecycleOwner, Observer { controlChanges ->
             val hasControlChanges = (controlChanges != null && controlChanges.isNotEmpty())
             viewBinding.hasControlChanges = hasControlChanges
             if (hasControlChanges)
                 listAdapter.submitList(controlChanges)
         })
+    }
+
+    class MidiInputPortObserver(private val listAdapter: ControlChangeAdapter): Observer<MidiInputPort> {
+        private var listener: InputPortListener? = null
+
+        override fun onChanged(inputPort: MidiInputPort?) {
+            if (inputPort == null) {
+                if (listener != null) listAdapter.removeOnControlChangeChangeValueListener(listener!!)
+                return
+            }
+            if (listener != null && listener?.inputPort == inputPort) {
+                return
+            }
+            listener = InputPortListener(inputPort)
+            listAdapter.addOnControlChangeChangeValueListener(listener!!)
+        }
+
+        class InputPortListener(val inputPort: MidiInputPort) : ControlChangeAdapter.OnControlChangeValueChangedListener {
+            override fun onControlChangeValueChanged(view: SeekBar, item: ControlChangeWithMidiChannel) {}
+
+            override fun onControlChangeValueChange(view: SeekBar, item: ControlChangeWithMidiChannel) {
+                val cc = ControlChangeMessage(
+                    item.midiChannel,
+                    item.controlChange.controllerNumber,
+                    item.controlChange.value
+                )
+                val bytes = cc.toByteArray()
+                inputPort.send(bytes, 0, bytes.size)
+            }
+
+        }
     }
 
 }
--- app/src/main/java/org/mitchwork/midi/viewmodels/AvailableMidiDevicesViewModel.kt
+++ app/src/main/java/org/mitchwork/midi/viewmodels/AvailableMidiDevicesViewModel.kt
@@ -2,31 +2,76 @@ package org.mitchwork.midi.viewmodels
 
 import android.bluetooth.le.ScanResult
 import android.media.midi.MidiDevice
+import android.media.midi.MidiDeviceInfo
+import android.media.midi.MidiInputPort
 import android.media.midi.MidiManager
+import android.os.Handler
+import android.os.Looper
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
 import org.mitchwork.midi.data.MidiDeviceTracker
 
 class AvailableMidiDevicesViewModel(private val midiManager: MidiManager) : ViewModel() {
     val availableDevices = AvailableMidiDevices(midiManager)
 
-    var midiDevice: LiveData<MidiDevice>? = null
+    fun refreshDevices() = availableDevices.refresh()
+
+    var midiDevice: MediatorLiveData<MidiDevice> = MediatorLiveData()
+
+    var midiInputPort: MediatorLiveData<MidiInputPort> = MediatorLiveData()
 
     fun addBluetoothResult(scanResult: ScanResult) = availableDevices.addBluetoothDevice(scanResult)
 
     fun openMidiDevice(device: MidiDeviceTracker) {
-        midiDevice = MidiDeviceLiveData(midiManager, device)
+        midiDevice.addSource(MidiDeviceLiveData(midiManager, device)) {
+            midiDevice.value = it
+        }
+    }
+
+    fun openFirstInputPort() {
+        if (midiInputPort.value != null) {
+            return
+        }
+        if (midiInputPort.value == null) {
+            midiInputPort.removeSource(midiDevice)
+        }
+        midiInputPort.addSource(midiDevice) {
+            device ->
+            val portNumber =
+                device.info.ports.find { it.type == MidiDeviceInfo.PortInfo.TYPE_INPUT }?.portNumber ?: -1
+            if (portNumber != -1) {
+                midiInputPort.value = device.openInputPort(portNumber)
+            }
+        }
     }
 
     class MidiDeviceLiveData(midiManager: MidiManager, device: MidiDeviceTracker) : LiveData<MidiDevice>() {
         init {
+            val handler = Handler(Looper.getMainLooper())
             if (device.midiDevice != null) {
-                midiManager.openDevice(device.midiDevice,
-                    { device -> value = device }, null)
+                midiManager.openDevice(
+                    device.midiDevice,
+                    { device ->
+                        value?.close()
+                        value = device
+                    }, handler
+                )
             } else if (device.bluetoothDevice != null) {
-                midiManager.openBluetoothDevice(device.bluetoothDevice.device,
-                    { device -> value = device }, null)
+                midiManager.openBluetoothDevice(
+                    device.bluetoothDevice.device,
+                    { device ->
+                        value?.close()
+                        value = device
+
+                    }, handler
+                )
             }
         }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        midiDevice?.value?.close()
     }
 }
