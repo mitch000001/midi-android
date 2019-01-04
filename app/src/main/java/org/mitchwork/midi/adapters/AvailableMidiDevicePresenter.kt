ae35101e108514dd1c6a35bf085cb40737e87cd0
Michael Wagner <mitch.wagna@gmail.com>
2019-01-04 13:22:38 +0100

    chore(viewmodel): refactor presenter

--- app/src/main/java/org/mitchwork/midi/adapters/AvailableMidiDevicePresenter.kt
+++ app/src/main/java/org/mitchwork/midi/adapters/AvailableMidiDevicePresenter.kt
@@ -3,21 +3,45 @@ package org.mitchwork.midi.adapters
 import android.media.midi.MidiDeviceInfo
 import org.mitchwork.midi.data.MidiDeviceTracker
 
-class AvailableMidiDevicePresenter(device: MidiDeviceTracker) {
+class AvailableMidiDevicePresenter(private val device: MidiDeviceTracker) {
     val name = device.midiDevice?.properties?.getString(MidiDeviceInfo.PROPERTY_NAME)
         ?: device.bluetoothDevice?.device?.name ?: ""
     val productName = device.midiDevice?.properties?.getString(MidiDeviceInfo.PROPERTY_PRODUCT)
         ?: device.bluetoothDevice?.device?.address ?: ""
     val manufacturer = device.midiDevice?.properties?.getString(MidiDeviceInfo.PROPERTY_MANUFACTURER)
         ?: ""
-    val inputPorts = "Input ports:\n" + (device.midiDevice?.ports
-        ?.filter { it.type == MidiDeviceInfo.PortInfo.TYPE_INPUT }
-        ?.map { it.name + " " + it.portNumber }
-        ?.reduce { acc, s ->  acc + "\n" + s }
-        ?: " - ")
-    val outputPorts = "Output ports:\n" + (device.midiDevice?.ports
-        ?.filter { it.type == MidiDeviceInfo.PortInfo.TYPE_OUTPUT }
-        ?.map { it.name + " " + it.portNumber }
-        ?.reduce { acc, s ->  acc + "\n" + s } ?: " - ")
-
+    val inputPorts: String
+        get() {
+            if (device.midiDevice != null) {
+                if (device.midiDevice.inputPortCount != 0) {
+                    return "Input ports:\n" + (device.midiDevice?.ports
+                        ?.filter { it.type == MidiDeviceInfo.PortInfo.TYPE_INPUT }
+                        ?.map { it -> run {
+                            if (it.name != "") {
+                                return it.name + " " + it.portNumber
+                            }
+                            return it.portNumber.toString()
+                        }}
+                        ?.joinToString("\n") ?: " - ")
+                }
+            }
+            return "Input ports: -"
+        }
+    val outputPorts: String
+        get() {
+            if (device.midiDevice != null) {
+                if (device.midiDevice.outputPortCount != 0) {
+                    return "Output ports:\n" + (device.midiDevice?.ports
+                        ?.filter { it.type == MidiDeviceInfo.PortInfo.TYPE_OUTPUT }
+                        ?.map { it -> run {
+                            if (it.name != "") {
+                                return it.name + " " + it.portNumber
+                            }
+                            return it.portNumber.toString()
+                        }}
+                        ?.joinToString("\n") ?: " - ")
+                }
+            }
+            return "Output ports: -"
+        }
 }
